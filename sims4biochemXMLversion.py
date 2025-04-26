1ef6758446856217bd85ab913388984842869d5e
demi <tuq92380@temple.edu>
2025-04-26 16:45:47 -0400

    Add files via upload

---
+++ sims4biochemXMLversion.py
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="utf-8"?>
+<I c="Career" i="career" m="careers.career_tuning" n="career_Biochemist" s="1234567890">
+  <base_tuning>career_Adult_Scientist</base_tuning>
+  
+  <career_name>BIOCHEMIST</career_name>
+  <career_description>A career in biochemistry: questionable pipetting, contaminated gels, and caffeine addiction. Will you survive?</career_description>
+
+  <career_levels>
+  
+    <CareerLevel>
+      <level>1</level>
+      <title>Pre-Lab Intern</title>
+      <description>Spends most of the day crying and pipetting incorrectly.</description>
+      <pay_per_hour>15</pay_per_hour>
+      <work_days>M,T,W,Th,F</work_days>
+      <start_hour>9</start_hour>
+      <end_hour>17</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>2</level>
+      <title>Gel Electrophoresis Enthusiast</title>
+      <description>Ruins gels like no other. Legendary for running samples off the plate entirely.</description>
+      <pay_per_hour>22</pay_per_hour>
+      <work_days>M,T,W,Th,F</work_days>
+      <start_hour>9</start_hour>
+      <end_hour>17</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>3</level>
+      <title>Caffeine-Enhanced Grad Student</title>
+      <description>Survives purely off of iced coffee and unearned optimism. Runs experiments while dissociating.</description>
+      <pay_per_hour>30</pay_per_hour>
+      <work_days>M,T,W,Th,F</work_days>
+      <start_hour>10</start_hour>
+      <end_hour>18</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>4</level>
+      <title>Chromatography Beginner</title>
+      <description>Lives and dies by HPLC results. Mostly dies.</description>
+      <pay_per_hour>45</pay_per_hour>
+      <work_days>M,T,W,Th,F</work_days>
+      <start_hour>9</start_hour>
+      <end_hour>17</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>5</level>
+      <title>Postdoc in Purgatory</title>
+      <description>You're technically employed... but at what cost?</description>
+      <pay_per_hour>55</pay_per_hour>
+      <work_days>M,T,W,Th,F</work_days>
+      <start_hour>9</start_hour>
+      <end_hour>17</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>6</level>
+      <title>Principal Investigator</title>
+      <description>Micromanages everyone else's experiments and hasn't touched a pipette in years.</description>
+      <pay_per_hour>75</pay_per_hour>
+      <work_days>M,T,W,Th</work_days>
+      <start_hour>8</start_hour>
+      <end_hour>16</end_hour>
+    </CareerLevel>
+
+    <CareerLevel>
+      <level>7</level>
+      <title>Biotechnician</title>
+      <description>Recognized in your field! But still can't afford a decent apartment.</description>
+      <pay_per_hour>100</pay_per_hour>
+      <work_days>M,T,W,Th</work_days>
+      <start_hour>8</start_hour>
+      <end_hour>16</end_hour>
+    </CareerLevel>
+
+  </career_levels>
+
+</I>
